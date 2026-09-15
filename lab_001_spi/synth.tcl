a99cf4e15e43fe32307e9b6ea11674b599bd5b48
klaee37 <k.noulkaew@gmail.com>
2026-09-15 17:36:06 +0700

    update pdk path

--- lab_001_spi/synth.tcl
+++ lab_001_spi/synth.tcl
@@ -3,10 +3,10 @@ set DESIGN			"spi"
 set DATE_SUFFIX		"[exec date +%m%d%y]_[exec date +%H%M]"
 set SUFFIX			"_${DATE_SUFFIX}"
 
-set LIB_LIST        { tcbn65lptc.lib }
+set LIB_LIST        { tcbn65lptc_ccs.lib }
 set RTL_LIST        { spi.v }
 
-set_db init_lib_search_path /home/icdesign/pdk/stclib/Front_End/timing_power_noise/NLDM/tcbn65lp_220a
+set_db init_lib_search_path /opt/cadence/PDK/TSMC65/digital/libraries/standard_cell/rvt/tcbn65lp_220a/ccs/
 set_db init_hdl_search_path $RTL_PATH
 
 suppress_messages { LBR-101 LBR-162 LBR-9 PHYS-12 LBR-155 LBR-415 PHYS-279 CWD-19 CWD-36 }
